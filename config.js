6a0cde54b480edee1026f4a1d7e872b476971140
xoton03 <sayahanis2003@gmail.com>
2026-06-20 18:23:44 +0100

    fix(prod): track config.js in git and remove config.prod.js workaround

---
+++ config.js
@@ -0,0 +1,7 @@
+// K.O.S.T. Configuration
+// Filled with API credentials from config.example.js
+window.KostConfig = {
+    SUPABASE_URL: "https://jphzmgscxpejcyjlnspq.supabase.co",
+    SUPABASE_KEY: "sb_publishable_gshF6Y08DYJYO9c8Z_Cv2Q_9nEZr7J9",
+    GAS_URL: "https://script.google.com/macros/s/AKfycbyLTx3UJtcZ96MNOYq7Kdkm8BDcOYzu-gLOkFDALPpdzrGmsKsUx_IdOZenLq8a0AdM-w/exec"
+};
